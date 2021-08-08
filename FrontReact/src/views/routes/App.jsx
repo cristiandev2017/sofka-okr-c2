f2f86010a6f9af929cd40d6f11f1cfc941cb488a
Cristian <camilo.santamaria322@gmail.com>
2021-08-08 16:06:22 -0500

    Se llena con informacion el listar y se obtiene el id del OKR con onchange

--- FrontReact/src/views/routes/App.jsx
+++ FrontReact/src/views/routes/App.jsx
@@ -1,27 +1,57 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from "react";
 //Redux
-import {connect} from "react-redux";
-import {bindActionCreators} from "redux";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 //Selectors
-//import { getOkrs } from './../../application/selectors/dashboard/okrs';
+import { getOkrs } from "./../../application/selectors/dashboard/okrs";
 //Acciones
-import {loadingOKR} from '../../application/actions/dashboard/index.js' 
+import { loadingOKR } from "../../application/actions/dashboard/index.js";
 
-function App({loadingOKR}) {
-    useEffect(()=>{
-        loadingOKR()
-    },[loadingOKR]);
-    return (
-        <div>
-            <h1>Componente de APP</h1>
-        </div>
-    )
+function App({ loadingOKR, okrs }) {
+  const [idokr, setidokr] = useState("");
+
+  useEffect(() => {
+    loadingOKR();
+  }, [loadingOKR]);
+  return (
+    <div>
+      <div className="row">
+        <center>
+          <div className="col-2"></div>
+          <div className="col-8">
+            <h1>Mis OKRs</h1>
+            <select
+              style={{width:"320px", height:"35px"}}
+              name="idokr"
+              value={idokr}
+              onChange={(e) => setidokr(e.target.value)}
+            >
+              {okrs.map((okr) => (
+                <option value={okr.id} key={okr.id}>{okr.title}</option>
+              ))}
+            </select>
+            <button className="btn btn-outline-warning">Ver info</button>
+          </div>
+          <div className="col-2"></div>
+        </center>
+      </div>
+    </div>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-    loadingOKR
-    },dispatch)
-}
+  return bindActionCreators(
+    {
+      loadingOKR,
+    },
+    dispatch
+  );
+};
 
-export default connect (null,mapDispatchToProps)(App);
+const mapStateToProps = (state) => {
+  return {
+    okrs: getOkrs(state),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
