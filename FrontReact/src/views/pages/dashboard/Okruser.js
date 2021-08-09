01eff2a989ded11c0739d27803ed89c6423bd24d
Cristian <camilo.santamaria322@gmail.com>
2021-08-09 15:46:08 -0500

    Se sube la lista de KRs

--- FrontReact/src/views/pages/dashboard/Okruser.js
+++ FrontReact/src/views/pages/dashboard/Okruser.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 //Circular Progressbar
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
@@ -10,21 +10,24 @@ function Okruser({ okr }) {
   let description = "";
   let areaInCharge = "";
   let progress = 0;
+  let krs = "";
   if (okr !== null) {
     progress = okr.progress;
     title = okr.title;
     description = okr.description;
     areaInCharge = okr.areaInCharge;
     objective = okr.objective;
-    console.log(progress);
+    krs = okr.krs;
   }
   return (
-     <Fragment>
+    <Fragment>
       {okr ? (
         <div>
+          {/*
           <div style={{ width: "16%" }}>
             <CircularProgressbar value={progress} text={`${progress}%`} />
           </div>
+            */}
           <div>
             <table className="table">
               <thead>
@@ -33,6 +36,7 @@ function Okruser({ okr }) {
                   <th scope="col">Descripcion</th>
                   <th scope="col">Area encargada</th>
                   <th scope="col">objetivo</th>
+                  <th scope="col">Progreso</th>
                 </tr>
               </thead>
               <tbody>
@@ -41,13 +45,68 @@ function Okruser({ okr }) {
                   <td>{description}</td>
                   <td>{areaInCharge}</td>
                   <td>{objective}</td>
+                  {/*<td>{(progress == 100) ?  "✅" : "❌" }</td>*/}
+                  {
+                    <td>
+                      <div style={{ width: "60%" }}>
+                        <CircularProgressbar
+                          value={progress}
+                          text={`${progress}%`}
+                        />
+                      </div>
+                    </td>
+                  }
                 </tr>
+              </tbody>
+            </table>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Descripcion KR</th>
+                  <th scope="col">Fecha de Inicio</th>
+                  <th scope="col">Fecha Final</th>
+                  <th scope="col">Porcentaje OKR</th>
+                  <th scope="col">Progreso</th>
+                </tr>
+              </thead>
+              <tbody>
+                {krs.map((kr) => (
+                  <tr>
+                    <td>{kr.description.value}</td>
+                    <td>{kr.startDate}</td>
+                    <td>{kr.endDate}</td>
+                    <td>
+                       {kr.loadValue}%
+                      <div className="progress">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          style={{width: `${kr.loadValue}%`}}
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
+                        </div>
+                      </div>
+                    </td>
+                    {/*<td>{(progress == 100) ?  "✅" : "❌" }</td>*/}
+                    {
+                      <td>
+                        <div style={{ width: "35%" }}>
+                          <CircularProgressbar
+                            value={kr.progress}
+                            text={`${kr.progress}%`}
+                          />
+                        </div>
+                      </td>
+                    }
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
       ) : (
-        "Seleccione un OKR"
+        <h6>Seleccione un OKR</h6>
       )}
     </Fragment>
   );
