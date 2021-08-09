bd0576dbe09216f97a4e57f2ae922b03e862cb7f
Cristian <camilo.santamaria322@gmail.com>
2021-08-09 11:55:51 -0500

    Se ponen endpoints de los servicios de la base de datos y se simula su informacion

--- FrontReact/src/infrastructure/services/api/dashboard/index.js
+++ FrontReact/src/infrastructure/services/api/dashboard/index.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 const functions = {
     loadingOKR: async () => {
-        const response = await axios.get('http://localhost:4000/OKRS');
+        const iduser = "61106133609d16f1740ddf34"
+        const response = await axios.get('https://grupo5-back-prueba.herokuapp.com/api/getokrbyuserid/'+iduser);
             return response.data
     },
+    //https://grupo5-back-prueba.herokuapp.com/api/getokrbyid/61109c8ea304d0a23abc347e
     loadingOKRid: async (id) =>{
-        const response = await axios.get('http://localhost:4000/OKRS?id='+id);
+        const response = await axios.get('https://grupo5-back-prueba.herokuapp.com/api/getokrbyid/'+id);
         return response.data
     }
 
--- FrontReact/src/views/pages/dashboard/Okruser.js
+++ FrontReact/src/views/pages/dashboard/Okruser.js
@@ -6,43 +6,45 @@ import "react-circular-progressbar/dist/styles.css";
 function Okruser({ okr }) {
   //Valido que vengan los datos
   let title = "";
-  let objetivo = "";
+  let objective = "";
   let description = "";
-  let areainCharge = "";
+  let areaInCharge = "";
   let progress = 0;
   if (okr !== null) {
-    progress = okr[0].progress;
-    title = okr[0].title;
-    description = okr[0].description;
-    areainCharge = okr[0].areainCharge;
-    objetivo = okr[0].objetivo;
+    progress = okr.progress;
+    title = okr.title;
+    description = okr.description;
+    areaInCharge = okr.areaInCharge;
+    objective = okr.objective;
+    console.log(progress);
   }
-
   return (
-    <Fragment>
+     <Fragment>
       {okr ? (
-        <div style={{ width: "16%" }}>
-          <CircularProgressbar value={progress} text={`${progress}%`} />
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Titulo</th>
-                <th scope="col">Descripcion</th>
-                <th scope="col">Area encargada</th>
-                <th scope="col">objetivo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>{title}</td>
-                <td>{description}</td>
-                <td>{areainCharge}</td>
-                <td>{objetivo}</td>
-              </tr>
-            </tbody>
-          </table>
+        <div>
+          <div style={{ width: "16%" }}>
+            <CircularProgressbar value={progress} text={`${progress}%`} />
+          </div>
+          <div>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Titulo</th>
+                  <th scope="col">Descripcion</th>
+                  <th scope="col">Area encargada</th>
+                  <th scope="col">objetivo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{title}</td>
+                  <td>{description}</td>
+                  <td>{areaInCharge}</td>
+                  <td>{objective}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       ) : (
         "Seleccione un OKR"
